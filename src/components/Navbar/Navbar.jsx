f8dc6b5e2163c74b42f0586f55f08524030d23cb
maria <maria.e.guimaraes19@aluno.senai.br>
2026-05-04 11:51:43 -0300

    Feat: Adiciona react-route

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,17 @@
+import styles from './Navbar.module.css';
+
+function Navbar() {
+  return (
+    <nav className={styles.navbar}>
+      <div className={styles.logo}>LOGO</div>
+      <ul className={styles.links}>
+        {/* Editaremos aqui */}
+        {/* Editaremos aqui */}
+        {/* Editaremos aqui */}
+        {/* Editaremos aqui */}
+        {/* Editaremos aqui */}
+      </ul>
+    </nav>
+  );
+}
+export default Navbar;
