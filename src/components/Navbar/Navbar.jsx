2f8737d25d6085a23f4fc6b4df62b3ee218966d1
Arthur Morais <mlkliso4000@gmail.com>
2026-05-11 14:10:43 -0300

    Feat: Adiciona NotFound

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import styles from './Navbar.module.css';
 
 function Navbar() {
@@ -5,11 +6,11 @@ function Navbar() {
     <nav className={styles.navbar}>
       <div className={styles.logo}>LOGO</div>
       <ul className={styles.links}>
-        {/* Editaremos aqui */}
-        {/* Editaremos aqui */}
-        {/* Editaremos aqui */}
-        {/* Editaremos aqui */}
-        {/* Editaremos aqui */}
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/sobre">Sobre</Link></li>
+        <li><Link to="/servicos">Serviços</Link></li>
+        <li><Link to="/contato">Contato</Link></li>
+        <li><Link to="/extra">Blog</Link></li>
       </ul>
     </nav>
   );
